9b462c6f1d3608f1526712b8d75e6c76c43078cf
deal <95deal@gmail.com>
2020-11-15 16:31:35 -0300

    created deal class and methods

---
+++ src/infrastructure/services/agendor/core/deals/Deal.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const { url, config } = require('../../../../http/api')
+
+module.exports = class Deal {
+  _httpClient
+
+  constructor(_httpClient) {
+    this._httpClient = _httpClient
+  }
+
+  async getDeals(id) {
+    const { data } = await this._httpClient.get(`${url}/deals/${id}`, config)
+    return data
+  }
+
+  async getDealsOfPerson(person_id) {
+    const { data } = await this._httpClient.get(`${url}/people/${person_id}/deals`, config)
+    return data
+  }
+
+  async getDealsOfOrganizations(organization_id) {
+    const { data } = await this._httpClient.get(`${url}/organizations/${organization_id}/deals`, config)
+    return data
+  }
+
+  async createDealForPerson(person_id, deal) {
+    const { data } = await this._httpClient.post(`${url}/people/${person_id}/deals`, deal, config)
+    return data
+  }
+
+  async createDealForOrganization(organization_id, deal) {
+    const { data } = await this._httpClient.post(`${url}/organizations/${organization_id}/deals`, deal, config)
+    return data
+  }
+
+  async updateDeal(id, deal) {
+    const { data } = await this._httpClient.put(`${url}/deals/${id}`, deal, config)
+    return data
+  }
+
+  async updateDealStatus(id, dealStatus) {
+    const { data } = await this._httpClient.put(`${url}/deals/${id}/status`, dealStatus, config)
+    return data
+  }
+
+  async updateDealStage(id, dealStage) {
+    const { data } = await this._httpClient.put(`${url}/deals/${id}/stage`, dealStage, config)
+    return data
+  }
+}
---
+++ src/infrastructure/services/agendor/core/deals/dealsRepository.js
@@ -0,0 +1,104 @@
+'use strict'
+
+const axios = require('axios')
+
+const { name } = require('faker')
+
+const Deal = require('./Deal')
+const deals = new Deal(axios)
+
+async function getDeals(id = '') {
+  try {
+    const deal = await deals.getDeals(id)
+    console.log(deal)
+  }
+  catch (err) {
+    console.error({ "Error": err.message })
+  }
+}
+
+async function getDealsOfPerson(person_id) {
+  try {
+    const dealsPerson = await deals.getDealsOfPerson(person_id)
+    console.log(dealsPerson)
+  }
+  catch (err) {
+    console.error({ "Error": err.message })
+  }
+}
+
+async function getDealsofOrganization(organization_id) {
+  try {
+    const dealsOrganization = await deals.getDealsOfOrganizations(organization_id)
+    console.log(dealsOrganization)
+  }
+  catch (err) {
+    console.error({ "Error": err.message })
+  }
+}
+
+async function createDealForPerson(person_id, deal) {
+  try {
+    const dealPerson = await deals.createDealForPerson(person_id, deal)
+    console.log(dealPerson)
+  }
+  catch (err) {
+    console.error({ "Error": err.message })
+  }
+}
+
+async function createDealForOrganization(organization_id, deal) {
+  try {
+    const dealOrganization = await deals.createDealForOrganization(organization_id, deal)
+    console.log(dealOrganization)
+  }
+  catch (err) {
+    console.error({ "Error": err.message })
+  }
+}
+
+async function updateDeal(id, deal) {
+  try {
+    const updateDeal = await deals.updateDeal(id, deal)
+    console.log(updateDeal)
+  }
+  catch (err) {
+    console.error({ "Error": err.message })
+  }
+}
+
+async function updateDealStatus(id, dealStatus) {
+  try {
+    const updateStatus = await deals.updateDealStatus(id, dealStatus)
+    console.log(updateStatus)
+  }
+  catch (err) {
+    console.error({ "Error": err.message })
+  }
+}
+
+async function updateDealStage(id, dealStage) {
+  try {
+    const updateStage = await deals.updateDealStage(id, dealStage)
+    console.log(updateStage)
+  }
+  catch (err) {
+    console.error({ "Error": err.message })
+  }
+}
+//getDeals('10756436')
+
+//getDealsOfPerson('33545094')
+
+//getDealsofOrganization('23387965')
+
+//createDealForPerson('33623986', { title: name.title() })
+
+//createDealForOrganization('23517322', { title: name.title() })
+
+//updateDeal('10756436', { description: name.jobDescriptor() })
+
+//dealStatusText = Enum: "ongoing" "won" "lost"
+//updateDealStatus('10756436', { dealStatusText: "won" })
+
+//updateDealStage('10756436', { dealStage: 3 })
