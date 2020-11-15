b9e856826d914142ae36436b6d8b9a0d7aecf1f2
deal <95deal@gmail.com>
2020-11-14 21:53:21 -0300

    created people class and methods

---
+++ src/infrastructure/services/agendor/core/people/People.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const { url, config } = require('../../../../http/api.js')
+
+module.exports = class Person {
+  _httpClient
+
+  constructor(_httpClient) {
+    this._httpClient = _httpClient
+  }
+
+  async getPeople(id) {
+    const { data } = await this._httpClient.get(`${url}/people/${id}`, config)
+    return data;
+  }
+
+  async createPerson(person) {
+    const { data } = await this._httpClient.post(`${url}/people`, person, config)
+    return data;
+  }
+
+  async upsertPerson(email) {
+    const { data } = await this._httpClient.post(`${url}/people/upsert`, email, config)
+    return data
+  }
+
+  async updatePerson(id, person) {
+    const { data } = await this._httpClient.put(`${url}/people/${id}`, person, config)
+    return data
+  }
+
+  async deletePerson(id) {
+    const { status } = await this._httpClient.delete(`${url}/people/${id}`, config)
+    return status
+  }
+}
---
+++ src/infrastructure/services/agendor/core/people/peopleRepository.js
@@ -0,0 +1,74 @@
+'use strict'
+
+const axios = require('axios')
+
+const { name, internet, company } = require('faker')
+
+const People = require('./People')
+const peopleClass = new People(axios)
+
+async function getPeople(id = '') {
+  try {
+    const people = await peopleClass.getPeople(id)
+    console.log(people)
+  }
+  catch (err) {
+    console.error({ "Error": err.message })
+  }
+}
+
+async function createPerson(person = '') {
+  try {
+    const createPerson = await peopleClass.createPerson(person)
+    console.log(createPerson)
+  }
+  catch (err) {
+    console.error({ "Error": err.message })
+  }
+}
+
+async function deletePerson(id) {
+  try {
+    const deletePerson = await peopleClass.deletePerson(id)
+    console.log(deletePerson)
+  }
+  catch (err) {
+    console.error({ "Error": err.message })
+  }
+}
+
+async function upsertPerson(email) {
+  try {
+    const upsertPerson = await peopleClass.upsertPerson(email)
+    console.log(upsertPerson)
+  }
+  catch (err) {
+    console.error({ "Error": err.message })
+  }
+}
+
+async function updatePerson(id, person) {
+  try {
+    const updatePerson = await peopleClass.updatePerson(id, person)
+    console.log(updatePerson)
+  }
+  catch (err) {
+    console.error({ "Error": err.message })
+  }
+}
+
+getPeople('33623986')
+
+createPerson({ name: name.firstName() })
+
+deletePerson('33623986')
+
+updatePerson('33623986', { contact: { email: internet.email() } })
+
+upsertPerson({
+  name: name.firstName(),
+  description: company.catchPhraseDescriptor(),
+  contact: {
+    email: 'Clement97@gmail.com'
+  }
+})
