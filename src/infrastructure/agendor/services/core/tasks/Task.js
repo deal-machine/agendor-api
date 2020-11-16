cb2d76484e0370bd4a3e206b7067fca981714e5e
deal <95deal@gmail.com>
2020-11-16 12:34:08 -0300

    refactored class task

--- src/infrastructure/agendor/services/core/tasks/Task.js
+++ src/infrastructure/agendor/services/core/tasks/Task.js
@@ -10,37 +10,72 @@ module.exports = class Task {
   }
 
   async getTasks() {
-    const { data } = await this._httpClient.get(`${url}/tasks`, config)
-    return data
+    try {
+      const { data } = await this._httpClient.get(`${url}/tasks`, config)
+      return data
+    }
+    catch (err) {
+      console.error({ "Error": err.message })
+    }
   }
 
   async getTasksOfPerson(person_id) {
-    const { data } = await this._httpClient.get(`${url}/people/${person_id}/tasks`, config)
-    return data
+    try {
+      const { data } = await this._httpClient.get(`${url}/people/${person_id}/tasks`, config)
+      return data
+    }
+    catch (err) {
+      console.error({ "Error": err.message })
+    }
   }
 
   async getTasksOfOrganization(organization_id) {
-    const { data } = await this._httpClient.get(`${url}/organizations/${organization_id}/tasks`, config)
-    return data
+    try {
+      const { data } = await this._httpClient.get(`${url}/organizations/${organization_id}/tasks`, config)
+      return data
+    }
+    catch (err) {
+      console.error({ "Error": err.message })
+    }
   }
 
   async getTasksOfDeal(deal_id) {
-    const { data } = await this._httpClient.get(`${url}/deals/${deal_id}/tasks`, config)
-    return data
+    try {
+      const { data } = await this._httpClient.get(`${url}/deals/${deal_id}/tasks`, config)
+      return data
+    }
+    catch (err) {
+      console.error({ "Error": err.message })
+    }
   }
 
   async createTaskForPerson(person_id, text) {
-    const { data } = await this._httpClient.post(`${url}/people/${person_id}/tasks`, text, config)
-    return data
+    try {
+      const { data } = await this._httpClient.post(`${url}/people/${person_id}/tasks`, text, config)
+      return data
+    }
+    catch (err) {
+      console.error({ "Error": err.message })
+    }
   }
 
   async createTaskForOrganization(organization_id, text) {
-    const { data } = await this._httpClient.post(`${url}/organizations/${organization_id}/tasks`, text, config)
-    return data
+    try {
+      const { data } = await this._httpClient.post(`${url}/organizations/${organization_id}/tasks`, text, config)
+      return data
+    }
+    catch (err) {
+      console.error({ "Error": err.message })
+    }
   }
 
   async createTaskForDeal(deals_id, text) {
-    const { data } = await this._httpClient.post(`${url}/deals/${deals_id}/tasks`, text, config)
-    return data
+    try {
+      const { data } = await this._httpClient.post(`${url}/deals/${deals_id}/tasks`, text, config)
+      return data
+    }
+    catch (err) {
+      console.error({ "Error": err.message })
+    }
   }
 }
--- src/tests/core/tasks/tasks.spec.js
+++ src/tests/core/tasks/tasks.spec.js
@@ -1,21 +1,21 @@
 'use strict'
 
-const { getTasks,
-  getTasksOfPerson,
-  getTasksofOrganization,
-  getTasksOfDeal } = require('../../../infrastructure/agendor/services/core/tasks/tasksRepository')
+const axios = require('axios')
+
+const Task = require('../../../infrastructure/agendor/services/core/tasks/Task')
+const task = new Task(axios)
 
 describe('Test Core Tasks', () => {
 
   it('should be able to get tasks', async () => {
-    const tasks = await getTasks()
+    const tasks = await task.getTasks()
 
     expect(tasks).toHaveProperty('data')
     expect(tasks).toHaveProperty('links')
   })
 
   it('should be able to get all tasks of a person', async () => {
-    const tasksOfPerson = await getTasksOfPerson('33623986')
+    const tasksOfPerson = await task.getTasksOfPerson('33623986')
 
     expect(tasksOfPerson).toHaveProperty('data')
     expect(tasksOfPerson).toHaveProperty('meta')
@@ -23,13 +23,13 @@ describe('Test Core Tasks', () => {
   })
 
   it('should be able to get all tasks of an organization', async () => {
-    const tasksOfOrganization = await getTasksofOrganization('23517322')
+    const tasksOfOrganization = await task.getTasksOfOrganization('23517322')
 
     expect(tasksOfOrganization).toHaveProperty('data')
   })
 
   it('should be able to get all tasks of a deal', async () => {
-    const tasksOfDeal = await getTasksOfDeal('10756436')
+    const tasksOfDeal = await task.getTasksOfDeal('10756436')
 
     expect(tasksOfDeal).toHaveProperty('data')
   })
